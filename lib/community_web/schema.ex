f5540535d0df21275d78134845f943318dc139c0
mdsebald <mdsebald@gmail.com>
2018-11-14 16:48:49 -0600

    Initial commit

---
+++ lib/community_web/schema.ex
@@ -0,0 +1,25 @@
+defmodule Community.Schema do
+
+@doc """
+  use Absinthe.Schema
+
+  alias CommunityWeb.NewsResolver
+
+  object :link do
+    field :id, non_null(:id)
+    field :url, non_null(:string)
+    field :description, non_null(:string)
+  end
+
+  query do
+    field :all_links, non_null(list_of(non_null(:link)))
+  end
+
+  field :all_links, non_null(list_of(non_null(:link))) do
+    resolve &NewsResolver.all_links/3
+  end
+
+end
+"""
+
+end
